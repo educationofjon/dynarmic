b4bd3ab089b02ee35a47e8616861a8eb55fb357d
Merry <git@mary.rs>
2022-05-21 16:48:22 +0100

    Squashed 'externals/mcl/' changes from 761b7c05e..8aeacfe32
    8aeacfe32 mcl: Increment version to 0.1.9
    b468a2ab5 mcl: meta_byte: Split off meta_byte_group
    d3ae1ae47 mcl: ihmap: Implement inline variant of hmap
    5cbfe6eed mcl: hmap: Split detail into headers
    ee7467677 mcl: hmap: Better default hash
    f1d902ce9 mcl: hash: Add xmrx
    322a221f0 mcl: hmap: Bugfix skip_empty_or_tombstone
    689f393f7 mcl: hmap: x64 implementation
    fa6ff746a mcl: hmap: Add generic meta_byte_group implementation
    91e3073ad mcl: hmap: Add more member functions
    4998335a5 mcl: Install only if master project
    7ff4d2549 mcl: hmap prototype
    416a2c6b5 mcl: clang-format: Adopt WebKit style bracing
    d5a46fa70 mcl/assert: Flush stderr
    e3b6cc79e externals: Update mcl to 0.1.7
    190c68475 mcl: Build as PIC
    
    git-subtree-dir: externals/mcl
    git-subtree-split: 8aeacfe329bdf6a9ea1261d002458a21cc80af2f

--- src/assert.cpp
+++ src/assert.cpp
@@ -11,9 +11,11 @@
 
 namespace mcl::detail {
 
-[[noreturn]] void assert_terminate_impl(fmt::string_view msg, fmt::format_args args) {
+[[noreturn]] void assert_terminate_impl(fmt::string_view msg, fmt::format_args args)
+{
     fmt::print(stderr, "assertion failed: ");
     fmt::vprint(stderr, msg, args);
+    std::fflush(stderr);
     std::terminate();
 }
 
